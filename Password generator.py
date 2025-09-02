f4e655d1d49930c1aa0d6a8dbc57ba78d297fcf6
FELIPE JIMENEZ JR <engrfelipejimenezjr@gmail.com>
2025-09-02 23:32:47 +0800

    Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -1,40 +1,34 @@
-import re
-import secrets
 import string
+import secrets
+import re
 
-
-def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
-
-    # Define the possible characters for the password
+def generate_password(length=16, nums=1, special_char=1, uppercase=1, lowercase=1):
+    
     letters = string.ascii_letters
-    digits = string.digits
     symbols = string.punctuation
-
-    # Combine all characters
-    all_characters = letters + digits + symbols
-
+    numbers = string.digits
+    
+    all_characters = letters + symbols + numbers
+    
     while True:
         password = ''
-        # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-        
+            
         constraints = [
             (nums, r'\d'),
-            (special_chars, fr'[{symbols}]'),
+            (special_char, '[{symbols}]'),
             (uppercase, r'[A-Z]'),
             (lowercase, r'[a-z]')
-        ]
-
-        # Check constraints        
+            ]
+            
         if all(
-            constraint <= len(re.findall(pattern, password))
+            constraint <= len(re.findall(pattern, password)) 
             for constraint, pattern in constraints
-        ):
+            ):
             break
-    
+        
     return password
-    
-if __name__ == '__main__':
-    new_password = generate_password()
-    print('Generated password:', new_password)
+
+password = generate_password()
+print(f'Generated Password: {password}')
